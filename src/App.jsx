22ff3ed6c5873246e8164f045b91c881ba5185e9
Ran Refael <ranrefae95@gmail.com>
2025-04-25 13:33:04 +0300

    update8

--- src/App.jsx
+++ src/App.jsx
@@ -8,40 +8,52 @@ function App() {
 
   return (
     <>
-    <div className='one'>
-      <header className="header-container" dir="rtl">
-        <h1>מעדני שנאל</h1>
-        <h2>מגשי אירוח יוקרתיים</h2>
-        <div className="kosher">כל המוצרים<br />בד"ץ</div>
-      </header>
+  <div className='one'>
+<header className="header-container" dir="rtl">
+  <h1>מעדני שנאל</h1>
+  <h2>מגשי אירוח יוקרתיים</h2>
+  <div className="kosher">כל המוצרים<br />בד"ץ</div>
+</header>
 
-      <div className="all">
-        <div className="menu">
-          <div className="section">
-            <h3>דגים |</h3>
-            <div className="item">פילה סלמון ברוטב מתקתק......................................................350₪</div>
-            <div className="item">פילה סלמון ברוטב פסטו עם פירורי לחם מעל...350₪</div>
-            <div className="item">פילה מושט ברוטב לימוני , עשבי טיבול ושום....350₪</div>
-            <div className="item">פילה דניס ברוטב לימוני................................450₪</div>
-          </div>
-        </div>
-
-        <div className="menu">
-          <div className="section">
-            <h3>יינות |</h3>
-            <div className="item">שורש אדום \ לבן | יקב צרעה................210₪</div>
-            <div className="item">הרי יהודה אדום \ לבן | יקב צרעה..........180₪</div>
-            <div className="item">עמוקה לבן.........................................100₪</div>
-          </div>
-        </div>
+<div className="all">
+  <div className="menu">
+  <div class="section">
+        <h3>מגשי אירוח</h3>
+        <div class="item">מגש גבינות מפנק ועשיר..............400₪</div>
+        <div class="item">מגש דגים מעושנים...........400₪</div>
+        <div class="item">מגש בורקיטס במילוי ממרח פסטו , בולגרית וחסה לאליק | 30 יחידות.........400₪</div>
+        <div class="item">טארטלים במילוי שמנת וסלמון | 30 יחידות......300₪</div>
+        <div class="item">מגש פריקסה | 25 יחידות...........450₪</div>
+        <div class="item">מגש פיתות סביח אישיות | 25 יחידות...........375₪</div>
+        <div class="item">מגש חצאי טורטיות במילוי סלט טונה \ ביצים | 20 יחידות............330₪</div>
       </div>
 
-      <footer>
-        <div>
-          050-2255425 | 052-3225482 <a href="https://instagram.com/Julias.bakery" target="_blank"></a>
-        </div>
-      </footer>
-    </div>
+
+  </div>
+
+  <div className="menu">
+
+
+
+
+      <div class="section">
+        <h3>בייגל בייגל</h3>
+        <div class="item">19 לחמניות בייגל עם ממרח פסטו , גבינה בולגרית ואנטי-פסטי..........330₪</div>
+        <div class="item">19 לחמניות בייגל במילוי סלט טונה מפנק וחסה לאליק...............330₪</div>
+        <div class="item">19 לחמניות בייגל במילוי סלט ביצים עם בצל ירוק וחסה לאליק...............330₪</div>
+        <div class="item">19 לחמניות בייגל במילוי ממרח שמנת עם דג סלמון ועגבנייה............400₪</div>
+      </div>
+
+
+  </div>
+</div>
+
+<footer>
+  <div>
+    050-2255425 | 052-3225482 <a href="https://instagram.com/Julias.bakery" target="_blank"></a>
+  </div>
+</footer>
+</div>
     </>
   )
 }
