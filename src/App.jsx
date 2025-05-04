1fb58260cb3b439a458241de21fd6f6eaf47982e
Ran Refael <ranrefae95@gmail.com>
2025-05-04 15:35:38 +0300

    update37

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import logo from "../logo/LL.png";
+import logo from "./LL.png";
 
 
 function App() {
